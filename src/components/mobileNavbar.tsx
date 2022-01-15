04c90bb4890fc30b3e0711874b045bb2ab29e613
BradyBassett <bradydbassett@gmail.com>
2022-01-14 23:44:02 -0600

    created burger

--- src/components/mobileNavbar.tsx
+++ src/components/mobileNavbar.tsx
@@ -8,13 +8,28 @@ const MobileNavbar: React.FC = () => {
             <h1 className="uppercase text-slate-200 self-center w-min mx-16 text-4xl font-semibold font-title">
                 imagetoascii.art
             </h1>
-            <ul className="absolute right-6 w-16 h-16 bg-slate-200 rounded-3xl self-center">
-                <div className="">
-                    <li className="bg-blue-600 w-12 h-2 rounded-md"></li>
-                    <li className="bg-blue-600 w-12 h-2 rounded-md"></li>
-                    <li className="bg-blue-600 w-12 h-2 rounded-md"></li>
+            <button
+                onClick={() => setIsOpen(!isOpen)}
+                className="absolute right-6 w-12 h-12 bg-slate-200 rounded-2xl self-center"
+            >
+                <div className="w-min mx-auto align-middle">
+                    <div
+                        className={`bg-blue-600 w-8 h-1 rounded-md my-1 transition-all ease-in duration-200 ${
+                            isOpen && "transform rotate-45 translate-y-2"
+                        }`}
+                    ></div>
+                    <div
+                        className={`bg-blue-600 w-8 h-1 rounded-md my-1 transition-all ease-out duration-300 ${
+                            isOpen && "transform translate-x-5 opacity-0"
+                        }`}
+                    ></div>
+                    <div
+                        className={`bg-blue-600 w-8 h-1 rounded-md my-1 transition-all ease-in duration-200 ${
+                            isOpen && "transform -rotate-45 -translate-y-2"
+                        }`}
+                    ></div>
                 </div>
-            </ul>
+            </button>
         </nav>
     );
 };
