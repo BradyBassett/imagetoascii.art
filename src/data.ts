d1beef74c35cc96980df5070e34489a9c64337e2
BradyBassett <bradydbassett@gmail.com>
2022-01-17 21:47:26 -0600

    data.ts init

---
+++ src/data.ts
@@ -0,0 +1,18 @@
+export const navList: { path: string; text: string }[] = [
+    {
+        path: "/",
+        text: "Home",
+    },
+    {
+        path: "/about",
+        text: "About",
+    },
+    {
+        path: "/feedback",
+        text: "Feedback",
+    },
+    {
+        path: "/privatePolicy",
+        text: "Private Policy",
+    },
+];
