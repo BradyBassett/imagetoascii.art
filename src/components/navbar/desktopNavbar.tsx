fc3e2d58a5eb187fa3e972b3a564a38ab9872917
BradyBassett <bradydbassett@gmail.com>
2022-01-21 00:21:50 -0600

    underlined current page

--- src/components/navbar/desktopNavbar.tsx
+++ src/components/navbar/desktopNavbar.tsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { navList } from "../../data";
 
+interface Ilink {
+    id: number;
+    value: boolean;
+}
+
 const DesktopNavbar: React.FC = () => {
+    const [isLinked, setIsLinked] = useState([
+        { id: 0, value: true },
+        { id: 1, value: false },
+        { id: 2, value: false },
+        { id: 3, value: false },
+    ]);
+
+    const handleSelectedLink = (index: number): void => {
+        setIsLinked(
+            isLinked.map((link): Ilink => {
+                if (link.id === index) {
+                    return { ...link, value: true };
+                } else {
+                    return { ...link, value: false };
+                }
+            })
+        );
+    };
+
     return (
         <>
             <ul className="hidden self-center text-slate-200 5xl:flex">
@@ -11,8 +35,11 @@ const DesktopNavbar: React.FC = () => {
                     return (
                         <li className="px-5 text-2xl 6xl:text-3xl 7xl:px-8" key={index}>
                             <Link
-                                className="transition-all ease-in duration-200 hover:border-b-2 hover:border-slate-200"
+                                className={`transition-all ease-in duration-200 hover:border-b-2 hover:border-slate-200${
+                                    isLinked[index].value ? " border-b-2 border-slate-200" : ""
+                                }`}
                                 to={path}
+                                onClick={() => handleSelectedLink(index)}
                             >
                                 {text}
                             </Link>
